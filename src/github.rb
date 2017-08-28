2b9d771734cb2b41e66e7b5a7164bb30862ae7cc
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-28 17:35:09 +0900

    GithubAPIからイベント一覧を取得するメソッドを実装

--- src/github.rb
+++ src/github.rb
@@ -1,3 +1,28 @@
+require 'json'
+require 'net/http'
+
 class Github
+
+  @@BASE_URL = "https://api.github.com"
+
+  def initialize(username)
+    @username = username
+  end
+
+  def events
+    url = "#{@@BASE_URL}/users/#{@username}/events"
+    get url
+  end
+
+  private
+
+  def get(url)
+    uri = URI.parse(url)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Get.new(uri.request_uri)
+    res = https.request(req)
+    JSON.parse(res.body)
+  end
 
 end
