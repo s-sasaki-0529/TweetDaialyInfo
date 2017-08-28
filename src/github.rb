32e5f3a35f568c7111517b47dad357a38b6f3b1c
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-28 17:53:33 +0900

    指定した日の総コミット数を取得するメソッドを実装

--- src/github.rb
+++ src/github.rb
@@ -6,25 +6,30 @@ class Github
 
   @@BASE_URL = "https://api.github.com"
 
-  def initialize(username)
+  def initialize(username, date = Date.today)
     @username = username
+    @events   = events_by_date(date)
   end
 
-  def events_by_date(date)
-    url = "#{@@BASE_URL}/users/#{@username}/events"
-    events = get url
-    events.select {|e| Date.parse(e['created_at']) == date}
+  def commit_count
+    @events.inject(0) {|sum, e| sum + e['payload']['commits'].length}
   end
 
   private
 
-  def get(url)
-    uri = URI.parse(url)
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Get.new(uri.request_uri)
-    res = https.request(req)
-    JSON.parse(res.body)
-  end
+    def events_by_date(date)
+      url = "#{@@BASE_URL}/users/#{@username}/events"
+      events = get url
+      events.select {|e| e['type'] === 'PushEvent' && Date.parse(e['created_at']) == date}
+    end
+
+    def get(url)
+      uri = URI.parse(url)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Get.new(uri.request_uri)
+      res = https.request(req)
+      JSON.parse(res.body)
+    end
 
 end
