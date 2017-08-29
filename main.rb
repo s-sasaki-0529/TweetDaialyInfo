5dc3191fe2b6d2d049d8572298a929ab491a3b54
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-29 21:55:36 +0900

    コミット行数をツイートできるように

--- main.rb
+++ main.rb
@@ -1,5 +1,22 @@
 require_relative 'src/twitter'
 require_relative 'src/zaim'
+require_relative 'src/github'
+
 today = Date.today
-todays_amount = Zaim.new.get_days_amount(today)
-Twitter.new.tweet("本日(#{today})の支出額は#{todays_amount}円です。 #ketilog")
+
+amounts = Zaim.new.get_days_amount(today)
+
+github  = Github.new('sa2knight')
+commits = github.commit_count
+lines   = github.total_added_lines
+
+Twitter.new.tweet(<<EOL)
+@null
+
+#{today.strftime('%Y-%m-%d')}
+
+支出額:   #{amounts}円
+Github: #{commits}コミット(#{lines}行)
+
+#ketilog
+EOL
