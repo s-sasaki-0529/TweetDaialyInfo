8494146a7df26e6880348a4f300de796f8f07754
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-08-29 22:54:43 +0900

    例外補足時にその内容をツイートするように

--- main.rb
+++ main.rb
@@ -2,23 +2,31 @@ require_relative 'src/twitter'
 require_relative 'src/zaim'
 require_relative 'src/github'
 
-today   = Date.today
-zaim    = Zaim.new
-twitter = Twitter.new
-github  = Github.new('Sa2Knight')
+begin
+  today   = Date.today
+  zaim    = Zaim.new
+  twitter = Twitter.new
+  github  = Github.new('Sa2Knight')
 
-Twitter.new.tweet(<<EOL)
-#{today.strftime('%Y-%m-%d')}
+  twitter.tweet(<<EOL)
+  #{today.strftime('%Y-%m-%d')}
 
-【Zaim】
-支出額:     #{zaim.get_days_amount(today)}円
+  【Zaim】
+  支出額:     #{zaim.get_days_amount(today)}円
 
-【Twitter】
-ツイート数: #{twitter.tweets_count(today)}
+  【Twitter】
+  ツイート数: #{twitter.tweets_count(today)}
 
-【Github】
-コミット数: #{github.commit_count}
-追加行数:   #{github.total_added_lines}行
+  【Github】
+  コミット数: #{github.commit_count}
+  追加行数:   #{github.total_added_lines}行
 
-#ketilog
+  #ketilog
 EOL
+rescue => e
+  Twitter.new.tweet(<<EOL)
+  #ketilog がバグりました
+
+  #{e}
+EOL
+end
