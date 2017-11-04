d2e5c13245d18e0d2e9148118c98e2b6a61d0d4e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-11-05 01:35:12 +0900

    技術ブログのアクセス数もツイートするように

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative 'src/twitter'
 require_relative 'src/zaim'
 require_relative 'src/github'
+require_relative 'src/analytics'
 
 begin
   today    = Date.today
@@ -10,21 +11,30 @@ begin
   payments = zaim.get_days_amount(today)
   budget   = zaim.get_current_month_private_budget
   contact  = Twitter.new.get_days_from_tweeted('コンタクト初日') + 1
+  sessions = Analytics.new('158527891').report_sessions_count_by_date(today)
 
   twitter.tweet(<<EOL)
 #{today.strftime('%Y-%m-%d')}
 
+[Zaim]
 支出[公費]: #{payments[:public]}円
 支出[私費]: #{payments[:private]}円
 予算[今月]: #{budget}円
 
-ツイート数: #{twitter.tweets_count(today)}
-コミット数: #{github.commit_count}
-
+[Twitter]
+ツイート: #{twitter.tweets_count(today)}
 コンタクト: #{contact}日目
+
+[Github]
+コミット: #{github.commit_count}
+
+[Analytics]
+ブログセッション: #{sessions[:total]}
 
 #ketilog
 EOL
+
+rescue => e
 
 rescue => e
   twitter.tweet(<<EOL)
