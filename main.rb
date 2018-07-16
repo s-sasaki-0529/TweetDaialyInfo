173b54d3ab40e773076d8710a662e032dc720668
Sa2Knight <shingo.sasaki.0529@gmail.com>
2018-07-16 17:31:14 +0900

    DMMの使用量もツイートするようにしつつツイート文を修正

--- main.rb
+++ main.rb
@@ -1,31 +1,38 @@
 require_relative 'src/twitter'
 require_relative 'src/zaim'
-require_relative 'src/github'
-require_relative 'src/analytics'
+require_relative 'src/dmm'
 
 begin
+  # スクリプト実行日
   today    = Date.today
-  github   = Github.new('Sa2Knight')
-  twitter  = Twitter.new
+
+  # ZaimAPI連携
   zaim     = Zaim.new
   payments = zaim.get_days_amount(today)
   budget   = zaim.get_current_month_private_budget
-  contact  = Twitter.new.get_days_from_tweeted('コンタクト初日') + 1
-  sessions = Analytics.new('158527891').report_sessions_count_by_date(today)
 
+  # DMM残容量をスクレイピング
+  dmm      = Dmm.new
+
+  # TwitterAPI連携
+  twitter  = Twitter.new
+  contact  = Twitter.new.get_days_from_tweeted('コンタクト初日') + 1
+
+  # ツイート内容の生成
   tweet_result = tweet_text = <<EOL
 #{today.strftime('%Y-%m-%d')}
 
-[Zaim]
 支出[公費]: #{payments[:public]}円
 支出[私費]: #{payments[:private]}円
 予算[今月]: #{budget}円
 
-[Twitter]
-ツイート: #{twitter.tweets_count(today)}
 コンタクト: #{contact}日目
+スマホ残容量: #{dmm.remaing_rate}%
+
+#ketilog
 EOL
 
+  # ツイートを投稿
   twitter.tweet(tweet_text[0, 140])
 
 rescue => e
---
+++ src/dmm.rb
@@ -0,0 +1,36 @@
+class Dmm
+  SCRIPT_PATH   = '/root/DmmMvnoChecker/main.py'.freeze
+  MAX_CAPACITY  = 3000.freeze
+
+  attr_reader :remaing_capacity
+
+  def initialize(remaing_capacity: nil)
+    @remaing_capacity = remaing_capacity
+    unless @remaing_capacity
+      script_result = `python #{SCRIPT_PATH}`
+      raise script_result unless script_result =~ /MB$/
+      @remaing_capacity = script_result.split('MB').first.tr(',', '').to_i
+    end
+  end
+
+  # 通信制限中か？
+  def used_up?
+    @remaing_capacity.zero?
+  end
+
+  # 使用量
+  def used_capacity
+    MAX_CAPACITY - @remaing_capacity
+  end
+
+  # 使用量の割合
+  def used_rate
+    ((used_capacity.to_f / MAX_CAPACITY.to_f) * 100).round(2)
+  end
+
+  # 残容量の割合
+  def remaing_rate
+    100.0 - used_rate
+  end
+
+end
