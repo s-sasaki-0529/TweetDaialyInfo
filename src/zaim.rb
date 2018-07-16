97b549e1f3b8ded7fc9017e5adf6336f461d0521
Sa2Knight <shingo.sasaki.0529@gmail.com>
2018-07-16 22:31:47 +0900

    zaimクラスをリファクタリング
    - APIの実行結果をインスタンス変数にキャッシュ
    - レシーバ経由で呼ばれないメソッドをprivateに
    - 使われていないメソッドを削除

--- src/zaim.rb
+++ src/zaim.rb
@@ -20,6 +20,7 @@ class Zaim
     @consumer = OAuth::Consumer.new(api_key["key"], api_key["secret"], oauth_params)
     @access_token = OAuth::AccessToken.new(@consumer, api_key["access_token"], api_key["access_token_secret"])
     @date = date
+    @payments_cach = {}
   end
 
   #
@@ -27,7 +28,7 @@ class Zaim
   # 公費とも私費とも取れない支払いがあった場合に例外を吐く
   #
   def get_days_amount(date , params = {})
-    payments = self.get_days_payments(date, params)
+    payments = get_days_payments(date, params)
     private_payments = payments.select{|payment| payment['comment'] =~ /私費/}
     public_payments  = payments.select{|payment| payment['comment'] =~ /公費/}
     unless (payments - private_payments - public_payments).empty?
@@ -46,57 +47,55 @@ class Zaim
   #
   def get_current_month_private_budget
     today = @date
-    payments = self.get_month_payments(today.year, today.month).select {|payment| payment['comment'] =~ /私費/}
+    payments = get_month_payments(today.year, today.month).select {|payment| payment['comment'] =~ /私費/}
     total_amount = payments.inject(0) {|sum, n| sum + n['amount']}
     return MONTHLY_BUDGET - total_amount
   end
 
-  #
-  # 指定した日付の支出一覧を取得
-  #
-  def get_days_payments(date, params = {})
-    date = date.strftime('%Y-%m-%d')
-    self.get_payments({
-      start_date: date,
-      end_date:   date
-    })
-  end
+  private
 
-  #
-  # 指定した月の支出一覧を取得
-  #
-  def get_month_payments(year, month, params = {})
-    self.get_payments({
-      start_date: Date.new(year, month),
-      end_date:   Date.new(year, month, -1)
-    })
-  end
+    #
+    # 指定した日付の支出一覧を取得
+    #
+    def get_days_payments(date, params = {})
+      date = date.strftime('%Y-%m-%d')
+      get_payments({
+        start_date: date,
+        end_date:   date
+      })
+    end
 
-  #
-  # 支出一覧を取得
-  #
-  def get_payments(params)
-    params.merge!({
-      'mode': 'payment'
-    })
-    url = Util.make_url("home/money" , params)
-    get(url)['money']
-  end
+    #
+    # 指定した月の支出一覧を取得
+    #
+    def get_month_payments(year, month, params = {})
+      get_payments({
+        start_date: Date.new(year, month),
+        end_date:   Date.new(year, month, -1)
+      })
+    end
 
-  #
-  # ZaimAPIに対してPOSTリクエストを送信
-  #
-  def get(url)
-    response = @access_token.get("#{API_URL}#{url}")
-    JSON.parse(response.body)
-  end
+    #
+    # 支出一覧を取得
+    # 同パラメータの場合はキャッシュを用いる
+    #
+    def get_payments(params)
+      cach_key = params.to_s
+      return @payments_cach[cach_key] if @payments_cach[cach_key]
 
-  #
-  # ZaimAPIに対してPUTリクエストを送信
-  #
-  def put(url , params = nil)
-    response = @access_token.put("#{API_URL}#{url}" , params)
-    JSON.parse(response.body)
-  end
+      params.merge!({
+        'mode': 'payment'
+      })
+      url = Util.make_url("home/money" , params)
+      @payments_cach[cach_key] = get(url)['money']
+    end
+
+    #
+    # ZaimAPIに対してPOSTリクエストを送信
+    #
+    def get(url)
+      response = @access_token.get("#{API_URL}#{url}")
+      JSON.parse(response.body)
+    end
 
 end
