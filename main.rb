89464557fa863d1f25a664abb7d3eaa274291983
Sa2Knight <shingo.sasaki.0529@gmail.com>
2018-07-30 00:14:17 +0900

    コマンドライン引数で相対日付を渡せるように

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require_relative 'src/fitbit'
 
 begin
   # スクリプト実行日
-  today    = Date.today
+  today    = Date.today + ARGV[0].to_i
   dow      = %w(日 月 火 水 木 金 土)[today.wday]
 
   # ZaimAPI連携
@@ -24,7 +24,7 @@ begin
 
   # FitbitAPI連携
   fitbit   = Fitbit.new
-  steps    = fitbit.fetch_steps_by_date(today)
+  # steps    = fitbit.fetch_steps_by_date(today)
 
   # ツイート内容の生成
   tweet_result = tweet_text = <<EOL
@@ -35,7 +35,6 @@ begin
 予算[今月]: #{budget}円
 
 昼食: #{lunch_place}
-歩数: #{steps}歩
 コンタクト: #{contact}日目
 散髪から: #{since_hair_cut}日目
 スマホ残容量: #{dmm.remaing_rate}%
